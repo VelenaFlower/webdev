8ae35426cd1e1714245c249472f2aed2f7ac60ee
Elena Yanchenko <veleno.ajt@gmail.com>
2020-11-11 22:17:41 +0300

    feat: add section community

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+  $('.community__slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: `<img class="slider-arrows slider-arrows__left" src="assets/images/arrow-prev.svg" alt="arrows">`,
+    nextArrow: `<div><img class="slider-arrows slider-arrows__right" src="assets/images/arrow-next.svg" alt="arrows"><div class="community__count"><span id="count">1</span>/5</div></div>`,
+  });
+  $(".community__slider").on('afterChange', function(event, slick, currentSlide){
+    $("#count").text(currentSlide + 1);
+  });
+
+});
