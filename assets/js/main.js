d531fada428c84c3f27f75b6df851d85b55f914e
Elena Yanchenko <veleno.ajt@gmail.com>
2020-11-13 01:48:49 +0300

    feat: add adaptive

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,16 @@ $(function () {
     slidesToScroll: 1,
     prevArrow: `<img class="slider-arrows slider-arrows__left" src="assets/images/arrow-prev.svg" alt="arrows">`,
     nextArrow: `<div><img class="slider-arrows slider-arrows__right" src="assets/images/arrow-next.svg" alt="arrows"><div class="community__count"><span id="count">1</span>/5</div></div>`,
+    responsive: [
+      {
+        breakpoint: 1221,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
   });
-  $(".community__slider").on('afterChange', function(event, slick, currentSlide){
+  $('.community__slider').on('afterChange', function(event, slick, currentSlide){
     $("#count").text(currentSlide + 1);
   });
 
